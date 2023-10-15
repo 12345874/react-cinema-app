b12aa1f4f46d33d8465c0e5a1aead5a7354e7cfb
RAHUL KUMAR <rahulkumar@RAHULs-MacBook-Air.local>
2023-10-15 16:16:14 +0530

    header  component added

---
+++ src/redux/reducer/errorReducer.js
@@ -0,0 +1,12 @@
+
+const initialState = {
+  message: '',
+  statusCode: null
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
